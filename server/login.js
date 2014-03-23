2a79046e67c4105b5c2299716c5d3866e1416041
Hugo Josefson <hugo@josefson.org>
2014-03-23 19:22:59 +0100

    Allow resume authentication.

--- server/login.js
+++ server/login.js
@@ -1,14 +1,26 @@
 Accounts.validateLoginAttempt(function (attempt) {
-    return (
-        attempt.allowed
-        && attempt.type === 'google'
-        && attempt.user
-        && attempt.user.services
-        && attempt.user.services.google
-        && attempt.user.services.google.email
-        && (
-        attempt.user.services.google.email === 'hugo@josefson.org'
-        || attempt.user.services.google.email === 'maria.kristina@josefson.org'
-        )
-        );
+
+    function isAllowedResume(attempt) {
+        return attempt.allowed && attempt.type === 'resume';
+    }
+
+    function isAnyGoogleAccount(attempt) {
+        return attempt.type === 'google'
+            && attempt.user
+            && attempt.user.services
+            && attempt.user.services.google
+            && attempt.user.services.google.email;
+    }
+
+    function isAllowedGoogleAccount(attempt) {
+        return attempt.allowed
+            && isAnyGoogleAccount(attempt)
+            && (
+                attempt.user.services.google.email === 'hugo@josefson.org'
+                || attempt.user.services.google.email === 'maria.kristina@josefson.org'
+                );
+
+    }
+
+    return isAllowedResume(attempt) || isAllowedGoogleAccount(attempt);
 });
